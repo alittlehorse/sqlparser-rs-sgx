32fb479bbb515ad1c80d54b433ebb09688076111
alittlehorse <my4993885@163.com>
2021-07-07 05:48:52 +0000

    print sql query

---
+++ enclave/src/lib.rs
@@ -0,0 +1,47 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License..
+
+#![crate_name = "sqlparserenclave"]
+#![crate_type = "staticlib"]
+
+#![cfg_attr(not(target_env = "sgx"), no_std)]
+#![cfg_attr(target_env = "sgx", feature(rustc_private))]
+
+extern crate sgx_types;
+#[cfg(not(target_env = "sgx"))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
+use sgx_types::*;
+use std::string::String;
+use std::vec::Vec;
+use std::io::{self, Write};
+use std::slice;
+
+#[no_mangle]
+pub extern "C" fn lexer(sql: *const u8, sql_len: usize) -> sgx_status_t {
+
+    println!("{}", "we are in Enclave now");
+    let str_slice = unsafe { slice::from_raw_parts(sql, sql_len) };
+    //let _ = io::stdout().write(str_slice);
+
+    // Construct a string from &'static string
+    let mut query = String::from_utf8_lossy(str_slice);
+    println!("{}", &query);
+
+    sgx_status_t::SGX_SUCCESS
+}
